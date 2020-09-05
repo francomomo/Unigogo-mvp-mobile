95bcd1f69be6290165a356a1aeb28b778e15f825
Ka Chun (Franco) Mo <46771981+francomomo@users.noreply.github.com>
2020-09-05 01:17:41 -0400

    Create account screen

---
+++ src/screens/AccountScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Text, StyleSheet } from 'react-native';
+
+const AccountScreen = () => {
+  return <Text style={styles.textStyle}>An Account Screen</Text>;
+};
+
+const styles = StyleSheet.create({
+  textStyle: {
+    fontSize: 30,
+  },
+});
+
+export default AccountScreen;
