fdf5eeceab30602afcb4af818890c67ac9852434
Ka Chun (Franco) Mo <46771981+francomomo@users.noreply.github.com>
2020-09-05 01:05:13 -0400

    First commit

--- App.js
+++ App.js
@@ -2,11 +2,15 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import { SignupScreen } from './src/screens/SignupScreen';
+import { LoginScreen } from './src/screens/LoginScreen';
+
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Hi</Text>
+      <StatusBar style='auto' />
     </View>
   );
 }
---
+++ src/screens/LoginScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Text, StyleSheet } from 'react-native';
+
+const LoginScreen = () => {
+  return <Text style={styles.textStyle}>A Login Screen</Text>;
+};
+
+const styles = StyleSheet.create({
+  textStyle: {
+    fontSize: 30,
+  },
+});
+
+export default LoginScreen;
